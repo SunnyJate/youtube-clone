59891cd4a3eddd72e318e97e1a79c1dc80993f7f
Sunny Jate <sunnyjate2007@gmail.com>
2024-09-20 21:51:01 +0530

    Updated sidebar to Ul

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,50 +12,62 @@ import { LuThumbsUp } from "react-icons/lu";
 export default function Sidebar() {
   const mainLinks = [
     {
-      icon: <MdHomeFilled />,
+      icon: <MdHomeFilled className="text-xl"/>,
       name: "Home",
     },
     {
-      icon: <SiYoutubeshorts />,
+      icon: <SiYoutubeshorts className="text-xl"/>,
       name: "Shorts",
     },
     {
-      icon: <MdSubscriptions />,
+      icon: <MdSubscriptions className="text-xl"/>,
       name: "Subscriptions",
     },
   ];
 const secondaryLinks = [{
-    icon: <MdOutlineVideoLibrary />,
+    icon: <MdOutlineVideoLibrary className="text-xl"/>,
     name: "Library",
 },
 {
-    icon: <MdHistory/>,
+    icon: <MdHistory className="text-xl"/>,
     name: "History",
 },
 {
-    icon: <MdOutlineWatchLater/>,
+    icon: <MdOutlineWatchLater className="text-xl"/>,
     name: "Watch Later",
 },
 {
-    icon: <LuThumbsUp/>,
+    icon: <LuThumbsUp className="text-xl"/>,
     name: "Liked Videos",
 }
 ]
   return(
-    <div className="sidebar w-2/12 bg-[#212121] pr-5 overflow-auto pb-8 h-screen">
-        <ul className="flex flex-col border-b-1 bg-grey-800">
+    <div className="sidebar w-2/12 bg-[#212121] p-2 pr-5 overflow-auto pb-8 h-screen">
+        <ul className="flex flex-col border-b-2 border-gray-700">   
             {mainLinks.map(({icon, name}) => {
                 return (
-                    <li key={name} className='pl-6 py-3 hover:bg-zinc-600 ${name === "Home"}'>
-                        <a href="#" className="link">
+                    <li key={name} className={'pl-6 py-3 hover:bg-zinc-600 ${name === "Home" "bg-slate-600":" "} rounded-xl'}>
+                        <a href="#" className="flex item-center gap-5">
                             {icon}
-                            <span>{name}</span>
+                            <span className="text-sm tracking-wider">{name}</span>
                         </a>
                     </li>
                     )
                 })
             }
-
+        </ul>
+        <ul className="flex flex-col border-b-1 border-gray-800">
+            {secondaryLinks.map(({icon, name}) => {
+                return (
+                    <li key={name} className={'pl-6 py-3 hover:bg-zinc-600 ${name === "Home" "bg-slate-600":" "} rounded-xl'}>
+                        <a href="#" className="flex item-center gap-5">
+                            {icon}
+                            <span className="text-sm tracking-wider">{name}</span>
+                        </a>
+                    </li>
+                    )
+                })
+            }
         </ul>
     </div>
   )
